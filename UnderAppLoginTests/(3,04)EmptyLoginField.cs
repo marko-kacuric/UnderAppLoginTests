3edc1ad395e7adba64ea8085a61681d9bc9aad1a
Kacuric <kci@p3-group.com>
2017-11-22 10:09:00 +0100

    test fix

--- UnderAppLoginTests/(3,04)EmptyLoginField.cs
+++ UnderAppLoginTests/(3,04)EmptyLoginField.cs
@@ -17,7 +17,7 @@ namespace UnderAppLoginTests
 
             Thread.Sleep(500);
             Browser.WaitForElement("/html/body/under-agent/under-login/div/under-modal-dialog[1]/div/div/div/div");
-            string alertTitle = "Please enter valid email!";
+            string alertTitle = "Please enter valid email";
             string alertMsg = Browser.Driver.FindElement(By.XPath("/html/body/under-agent/under-login/div/under-modal-dialog[1]/div/div/div/div/div[2]")).Text;
             string issueMessage = "@---------------------@Wrong error message is presented!@Expected result: " + alertTitle + "@Actual result: " + alertMsg + "@---------------------";
             issueMessage = issueMessage.Replace("@", "\n");
